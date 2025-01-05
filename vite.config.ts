898c382020ad765e302c97f7db75af1a266fcca5
jose <josemalheiro105@gmail.com>
2025-01-05 08:34:23 -0800

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import svgrPlugin from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Use the PORT environment variable
+  },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   build: {
     outDir: 'build',
